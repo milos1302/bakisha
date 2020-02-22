5704ec8421c098c1ef008fb84e75aaee3e76da2e
Milos Roknic <m.roknic@vegaitsourcing.rs>
2020-02-22 23:58:46 +0100

    Add the list view to 'Profile' model

--- app/user/urls.py
+++ app/user/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.PostListView.as_view(), name='profile_list'),
     path('<int:pk>', views.ProfileDetailView.as_view(), name='profile_detail'),
 ]
--- app/user/views.py
+++ app/user/views.py
@@ -1,6 +1,10 @@
-from django.views.generic import DetailView
+from django.views.generic import DetailView, ListView
 from .models import Profile
 
 
 class ProfileDetailView(DetailView):
+    model = Profile
+
+
+class PostListView(ListView):
     model = Profile
