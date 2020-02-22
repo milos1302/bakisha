9e7da90cd72b7bcd8facefaeaeebf395195ac9fa
Milos Roknic <m.roknic@vegaitsourcing.rs>
2020-02-23 00:04:56 +0100

    Connect profile_list items with profile_detail

--- app/user/urls.py
+++ app/user/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.PostListView.as_view(), name='profile_list'),
-    path('<int:pk>', views.ProfileDetailView.as_view(), name='profile_detail'),
+    path('', views.PostListView.as_view(), name='profile-list'),
+    path('<int:pk>', views.ProfileDetailView.as_view(), name='profile-detail'),
 ]
