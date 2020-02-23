0abb5f29006c2da47386f766af97b73cc52b140d
Milos Roknic <m.roknic@vegaitsourcing.rs>
2020-02-23 15:04:31 +0100

    Add Profile update forms

--- app/user/views.py
+++ app/user/views.py
@@ -3,7 +3,7 @@ from django.contrib import messages
 from django.shortcuts import redirect, render
 from django.contrib.auth.decorators import login_required
 from .models import Profile
-from .forms import UserSignupForm
+from .forms import UserSignupForm, UserUpdateForm, ProfileUpdateForm
 
 
 def signup(request):
@@ -21,7 +21,24 @@ def signup(request):
 
 @login_required
 def my_profile(request):
-    return render(request, 'user/my_profile.html')
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f'Your account has been updated!')
+            return redirect('my-profile')
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        'user_form': user_form,
+        'profile_form': profile_form
+    }
+
+    return render(request, 'user/my_profile.html', context)
 
 
 class ProfileDetailView(DetailView):
