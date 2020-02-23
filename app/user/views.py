66655ea38f9ad79a458ad868fb84fc815b93c5fc
Milos Roknic <m.roknic@vegaitsourcing.rs>
2020-02-23 09:24:08 +0100

    Add title to detail and list views for Profile

--- app/user/views.py
+++ app/user/views.py
@@ -5,6 +5,13 @@ from .models import Profile
 class ProfileDetailView(DetailView):
     model = Profile
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        username = context['profile'].user.username
+        context['title'] = f"{username}'s profile"
+        return context
+
 
 class ProfileListView(ListView):
     model = Profile
+    extra_context = {'title': 'Players'}
