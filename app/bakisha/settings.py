e0b16993a24bf57724211d184520acfdf535663e
Milos Roknic <m.roknic@vegaitsourcing.rs>
2020-02-23 15:08:34 +0100

    Create 'game' app

--- app/bakisha/settings.py
+++ app/bakisha/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'user.apps.UserConfig',
+    'game.apps.GameConfig',
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
