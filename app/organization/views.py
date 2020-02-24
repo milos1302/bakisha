ab0d2ccc7be33cd06d74ba1da9b73593aab09a3b
Milos Roknic <m.roknic@vegasourcing.rs>
2020-02-24 15:46:15 +0100

    Add 'templatetags' to organization app and display 'created by (username)' in organization templates

--- app/organization/views.py
+++ app/organization/views.py
@@ -6,9 +6,20 @@ from .models import Organization
 class OrganizationDetailView(DetailView):
     model = Organization
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        if context['organization'].created_by:
+            context['created_by'] = context['organization'].created_by.username
+        return context
+
 
 class OrganizationListView(ListView):
     model = Organization
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        print(context['organization_list'][1].created_by)
+        return context
 
 
 class OrganizationCreateView(LoginRequiredMixin, CreateView):
