1e008ad2f56444bb95c039a0f17e8ddd75adcd34
Milos Roknic <m.roknic@vegasourcing.rs>
2020-02-24 11:59:21 +0100

    Display players on Game detail view

--- app/game/views.py
+++ app/game/views.py
@@ -9,6 +9,11 @@ class GameListView(ListView):
 class GameDetailView(DetailView):
     model = Game
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['players'] = self.object.players.all()
+        return context
+
 
 class GameCreateView(CreateView):
     model = Game
