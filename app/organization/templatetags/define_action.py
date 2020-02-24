6beb29411af40a351beab680f0a73ca390a91ca7
Milos Roknic <m.roknic@vegasourcing.rs>
2020-02-24 16:52:49 +0100

    Create 'in_group' template tag (fitler)

--- app/organization/templatetags/define_action.py
+++ app/organization/templatetags/define_action.py
@@ -1,7 +1,7 @@
 from django import template
 
-
 register = template.Library()
+
 
 @register.simple_tag
 def define(value=''):
--- app/organization/views.py
+++ app/organization/views.py
@@ -18,7 +18,6 @@ class OrganizationListView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        print(context['organization_list'][1].created_by)
         return context
 
 
