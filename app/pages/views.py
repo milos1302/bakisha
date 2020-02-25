4f6498ed3aa491ad8a7380624d5aef6a7a25e684
Milos Roknic <m.roknic@vegasourcing.rs>
2020-02-25 16:06:50 +0100

    Add custom 403 handler

--- app/pages/views.py
+++ app/pages/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'pages/home.html', {'title': 'Home'})
+
+
+def handler403(request, exception=None):
+    return render(request, 'pages/403.html', {
+        'title': 'Denied', 'message': 'You do not have permission to perform this action!'
+    })
