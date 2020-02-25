057cb6a5d1b29aea3a4c872cf6948d530023e63c
Milos Roknic <m.roknic@vegasourcing.rs>
2020-02-25 13:24:58 +0100

    Make 'user' field in Profile required

--- app/user/models.py
+++ app/user/models.py
@@ -4,7 +4,7 @@ from common.utils.images import resize_image
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default='images/user/profile/default.png', upload_to='images/user/profile')
 
     def __str__(self):
