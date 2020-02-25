7baa55c2c86a98a20987364d89bb9bf58a306d6f
Milos Roknic <m.roknic@vegasourcing.rs>
2020-02-25 17:08:18 +0100

    Prevent adding non-administrators to organization's administrators

--- app/organization/admin.py
+++ app/organization/admin.py
@@ -1,4 +1,19 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 from .models import Organization
 
-admin.site.register(Organization)
+
+class OrganizationAdmin(admin.ModelAdmin):
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        """
+        Prevent adding non-administrators to organization's administrators.
+        Only users in 'Administrators' group can be added as organization's
+        administrators.
+        """
+        if db_field.name == 'administrators':
+            kwargs['queryset'] = User.objects.filter(groups__name__in=['Administrators'])
+        return super().formfield_for_manytomany(db_field, request, **kwargs)
+
+
+admin.site.register(Organization, OrganizationAdmin)
