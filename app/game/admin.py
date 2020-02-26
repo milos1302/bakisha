5963065ae961443831330d564fb990eedea8b199
Milos Roknic <m.roknic@vegasourcing.rs>
2020-02-26 18:03:14 +0100

    Make 'created_by', 'type' and 'organization' fields READ ONLY in admin form for Game model

--- app/game/admin.py
+++ app/game/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import GameType, Game
 
+
+class GameAdmin(admin.ModelAdmin):
+    readonly_fields = ['created_by', 'type', 'organization']
+
+
 admin.site.register(GameType)
-admin.site.register(Game)
+admin.site.register(Game, GameAdmin)
